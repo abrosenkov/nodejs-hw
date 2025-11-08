c657b64e046ef4ea329642e24777862617474652
abrosenkov <abrosenkov91@gmail.com>
2025-11-08 01:43:18 +0100

    add patch rout

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,12 +1,21 @@
-import { Router } from "express";
-import { createNote, getNoteById, getNotes } from "../controllers/notesController.js";
+import { Router } from 'express';
+import {
+  createNote,
+  getNoteById,
+  getAllNotes,
+  deleteNote,
+  updateNote,
+} from '../controllers/notesController.js';
 
 const router = Router();
 
-
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 
 router.post('/notes', createNote);
+
+router.delete('/notes/:noteId', deleteNote);
+
+router.patch('/notes/:noteId', updateNote);
 
 export default router;

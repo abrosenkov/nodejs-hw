e28cea91e8861a7eacd407e25eb271039cf1408f
abrosenkov <abrosenkov91@gmail.com>
2025-11-23 23:15:24 +0100

    add refresh rout

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { celebrate } from 'celebrate';
+import {
+  loginUserSchema,
+  registerUserSchema,
+} from '../validations/authValidation.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+
+router.post('/auth/refresh');
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ import 'dotenv/config';
 import helmet from 'helmet';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -23,6 +25,7 @@ app.use(express.json());
 
 // CORS  Middleware
 app.use(cors());
+app.use(cookieParser());
 
 // Time log  Middleware
 app.use((req, res, next) => {
@@ -31,6 +34,7 @@ app.use((req, res, next) => {
 });
 
 // Routers Middleware
+app.use(authRoutes);
 app.use(notesRoutes);
 
 // Not found Middleware
